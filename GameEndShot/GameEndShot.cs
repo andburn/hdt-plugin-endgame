62c3ab8ca580b47d32d884163b1fdaa437750eb6
Andrew Burnett <andrew@nospam.forme>
2015-04-10 16:40:25 +0100

    Add simple print screen simulation

---
+++ GameEndShot/GameEndShot.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Hearthstone_Deck_Tracker.Plugins;
+using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker;
+
+namespace AndBurn.HDT.Plugins.GameEndShot
+{
+    public class GameEndShotPlugin : IPlugin
+    {
+        public string Name
+        {
+            get { return "GameEndShot"; }
+        }
+
+        public string Description
+        {
+            get { return "Saves an image of the game end screen."; }
+        }
+
+        public string ButtonText
+        {
+            get { return "Nothing"; }
+        }
+
+        public string Author
+        {
+            get { return "andburn"; }
+        }
+
+        public Version Version
+        {
+            get { return new Version(0, 0, 1); }
+        }
+
+        public void OnLoad()
+        {
+            GameEvents.OnGameEnd.Add(TakeScreenshot);
+        }
+
+        public void OnUnload()
+        {
+            // Nothing for now
+        }
+
+        public void OnUpdate()
+        {
+            // Nothing for now
+        }
+
+        public void OnButtonPress()
+        {
+            // Nothing for now
+        }
+
+        private void TakeScreenshot()
+        {
+            System.Threading.Thread.Sleep(5000);
+            var wi = new WindowsInput.InputSimulator();
+            wi.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.SNAPSHOT);
+        }
+
+    }
+}
+
---
+++ GameEndShot/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GameEndShot")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GameEndShot")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fddf1e5e-7f13-4766-a3fe-3927d1d18040")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
