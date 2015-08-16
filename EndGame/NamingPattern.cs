ef686d277743e7dfc6e94c7962a10c5b2e045f4c
Andrew Burnett <andrew@nospam.forme>
2015-08-16 23:28:03 +0100

    Add naming pattern class and tests

---
+++ EndGame/NamingPattern.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HDT.Plugins.EndGame
+{
+	public class NamingPattern
+	{
+		private enum Token
+		{
+			PlayerName,
+			PlayerClass,
+			OpponentName,
+			OpponentClass,
+			Date
+		}
+
+		private NamingPattern()
+		{
+
+		}
+
+		private NamingPattern(string pattern)
+		{
+
+		}
+
+		public static bool TryParse(string pattern, out NamingPattern naming)
+		{
+			const string regex = @"^[^{}]*({[A-Z][^{}]+}[^{}]*)*$";
+			naming = null;
+
+			var match = Regex.Match(pattern, regex);
+			if(match.Success)
+			{
+				return true;
+			}
+			return false;
+		}
+
+	}
+}
---
+++ EndGameTests/NamingPatternTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HDT.Plugins.EndGame.Tests
+{
+	[TestClass]
+	public class NamingPatternTest
+	{
+		private NamingPattern np;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			np = null;
+		}
+
+		//[TestMethod]
+		//public void TestUnknownToken()
+		//{
+		//	Assert.IsFalse(NamingPattern.TryParse("{PlayerRank}", out np));
+		//}
+
+		//[TestMethod]
+		//public void TestKnownToken()
+		//{
+		//	Assert.IsTrue(NamingPattern.TryParse("{PlayerName}", out np));
+		//}
+
+		[TestMethod]
+		public void TestValidSingleTokenPattern()
+		{
+			Assert.IsTrue(NamingPattern.TryParse("{PlayerName}", out np));
+		}
+
+		[TestMethod]
+		public void TestValidMultiTokenPattern()
+		{
+			Assert.IsTrue(NamingPattern.TryParse("Prefix {PlayerName} vs {OpponentName} Postfix", out np));
+		}
+
+		[TestMethod]
+		public void TestInValidTokenPattern()
+		{
+			Assert.IsFalse(NamingPattern.TryParse("{nolower}", out np));
+		}
+
+		[TestMethod]
+		public void TestPatternWithNoTokens()
+		{
+			Assert.IsTrue(NamingPattern.TryParse("Just a String", out np));
+		}
+	}
+}
