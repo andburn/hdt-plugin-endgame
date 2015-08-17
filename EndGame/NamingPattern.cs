77a2ea0a737be25a4db364e6c84e132d4f967d78
Andrew Burnett <andrew@nospam.forme>
2015-08-17 14:29:07 +0100

    Naming pattern complete and tested

--- EndGame/NamingPattern.cs
+++ EndGame/NamingPattern.cs
@@ -4,41 +4,107 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Hearthstone_Deck_Tracker.Stats;
 
 namespace HDT.Plugins.EndGame
 {
 	public class NamingPattern
 	{
-		private enum Token
-		{
-			PlayerName,
-			PlayerClass,
-			OpponentName,
-			OpponentClass,
-			Date
-		}
-
 		private NamingPattern()
 		{
-
+			Pattern = new List<string>();
 		}
 
-		private NamingPattern(string pattern)
-		{
-
-		}
+		public List<string> Pattern { get; private set; }
 
 		public static bool TryParse(string pattern, out NamingPattern naming)
 		{
-			const string regex = @"^[^{}]*({[A-Z][^{}]+}[^{}]*)*$";
-			naming = null;
+			const string regex = @"^([^{}]*)(({[A-Z][^{}]+})([^{}]*))*$";
+			naming = new NamingPattern();
+
+			Regex r = new Regex(regex);
 
 			var match = Regex.Match(pattern, regex);
 			if(match.Success)
 			{
+				var prefix = match.Groups[1].Captures[0].Value;
+				if (!String.IsNullOrEmpty(prefix))
+					naming.Pattern.Add(prefix);
+
+				var tokens = match.Groups[3].Captures;
+				var strings = match.Groups[4].Captures;
+
+				// TODO: check tokens.count == strings.count
+				for(int i = 0; i < tokens.Count; i++)
+				{
+					naming.Pattern.Add(tokens[i].Value);
+					if(!String.IsNullOrEmpty(strings[i].Value))
+						naming.Pattern.Add(strings[i].Value);
+				}
+
 				return true;
 			}
 			return false;
+		}
+
+		public string Apply(GameStats game)
+		{
+			var name = "";
+			foreach(var token in Pattern)
+			{
+				var tokenLower = token.ToLower();
+				if(tokenLower == "{playerclass}")
+				{
+					name += game.PlayerHero;
+				}
+				else if(tokenLower == "{opponentclass}")
+				{
+					name += game.OpponentHero;
+				}
+				else if(tokenLower == "{playername}")
+				{
+					name += game.PlayerName;
+				}
+				else if(tokenLower == "{opponentname}")
+				{
+					name += game.OpponentName;
+				}
+				else if(tokenLower.Contains("{date:"))
+				{
+					name += ParseDate(token);
+				}
+				else
+				{
+					name += token;
+				}
+			}
+			return name;
+		}
+
+		private string ParseDate(string format)
+		{
+			var defaultDate = DateTime.Now.ToString("dd.MM.yyyy_HH.mm");
+			try
+			{
+				const string regex = "{[Dd]ate:(?<date>(.*?))}";
+				var match = Regex.Match(format, regex);
+				if(match.Success)
+				{
+					var capture = match.Groups["date"].Value;
+					var date = DateTime.Now.ToString(capture);
+					Console.WriteLine(date);
+					if(date == capture)
+						return defaultDate;
+					else
+						return date;
+				}
+				return defaultDate;
+			}
+			catch
+			{
+				// error parsing date, use default format
+				return defaultDate;
+			}
 		}
 
 	}
--- EndGameTests/NamingPatternTest.cs
+++ EndGameTests/NamingPatternTest.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using Hearthstone_Deck_Tracker.Enums;
+using Hearthstone_Deck_Tracker.Stats;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HDT.Plugins.EndGame.Tests
@@ -7,47 +10,106 @@ namespace HDT.Plugins.EndGame.Tests
 	public class NamingPatternTest
 	{
 		private NamingPattern np;
+		private GameStats game;
 
 		[TestInitialize]
 		public void Setup()
 		{
 			np = null;
+			game = new GameStats(GameResult.Loss, "Warlock", "Mage");
+			game.PlayerName = "Player1";
+			game.OpponentName = "Player2";
 		}
 
-		//[TestMethod]
-		//public void TestUnknownToken()
-		//{
-		//	Assert.IsFalse(NamingPattern.TryParse("{PlayerRank}", out np));
-		//}
-
-		//[TestMethod]
-		//public void TestKnownToken()
-		//{
-		//	Assert.IsTrue(NamingPattern.TryParse("{PlayerName}", out np));
-		//}
-
 		[TestMethod]
-		public void TestValidSingleTokenPattern()
+		public void SingleTokenPattern()
 		{
 			Assert.IsTrue(NamingPattern.TryParse("{PlayerName}", out np));
 		}
 
 		[TestMethod]
-		public void TestValidMultiTokenPattern()
+		public void MultiTokenPattern()
+		{
+			Assert.IsTrue(NamingPattern.TryParse("{PlayerName}{PlayerClass}", out np));
+		}
+
+		[TestMethod]
+		public void MultiTokenPlusStringsPattern()
 		{
 			Assert.IsTrue(NamingPattern.TryParse("Prefix {PlayerName} vs {OpponentName} Postfix", out np));
 		}
 
 		[TestMethod]
-		public void TestInValidTokenPattern()
+		public void PatternWithNoTokens()
+		{
+			Assert.IsTrue(NamingPattern.TryParse("Just a String", out np));
+		}
+
+		[TestMethod]
+		public void EmptyPattern()
+		{
+			Assert.IsTrue(NamingPattern.TryParse("", out np));
+		}
+
+		[TestMethod]
+		public void InValidTokens()
 		{
 			Assert.IsFalse(NamingPattern.TryParse("{nolower}", out np));
 		}
 
 		[TestMethod]
-		public void TestPatternWithNoTokens()
+		public void ValidTokens()
 		{
-			Assert.IsTrue(NamingPattern.TryParse("Just a String", out np));
+			Assert.IsTrue(NamingPattern.TryParse("{PlayerName}", out np));
+		}
+
+		[TestMethod]
+		public void ParseTokensInCorrectOrder()
+		{
+			NamingPattern.TryParse("{PlayerName}{PlayerClass}", out np);
+			Assert.AreEqual(2, np.Pattern.Count);
+			Assert.AreEqual("{PlayerName}", np.Pattern[0]);
+			Assert.AreEqual("{PlayerClass}", np.Pattern[1]);
+		}
+
+		[TestMethod]
+		public void ParseFullPatternInCorrectOrder()
+		{
+			NamingPattern.TryParse(" Before {PlayerName} Middle {PlayerClass} After ", out np);
+			Assert.AreEqual(5, np.Pattern.Count);
+			Assert.AreEqual(" Before ", np.Pattern[0]);
+			Assert.AreEqual("{PlayerName}", np.Pattern[1]);
+			Assert.AreEqual(" Middle ", np.Pattern[2]);
+			Assert.AreEqual("{PlayerClass}", np.Pattern[3]);
+			Assert.AreEqual(" After ", np.Pattern[4]);
+		}
+
+		[TestMethod]
+		public void PatternEvaluatesCorrectly()
+		{
+			NamingPattern.TryParse(" Before {PlayerName} ({PlayerClass}) VS {OpponentName} ({OpponentClass}) After ", out np);
+			Assert.AreEqual(" Before Player1 (Mage) VS Player2 (Warlock) After ", np.Apply(game));
+		}
+
+		[TestMethod]
+		public void ValidDatePattern()
+		{
+			NamingPattern.TryParse("{Date:ddMMyyy HH:mm}", out np);
+			Assert.AreEqual(DateTime.Now.ToString("ddMMyyy HH:mm"), np.Apply(game));
+		}
+
+		[TestMethod]
+		public void InValidDatePattern()
+		{
+			NamingPattern.TryParse("{Date:X}", out np);
+			Assert.AreEqual(DateTime.Now.ToString("dd.MM.yyy_HH.mm"), np.Apply(game));
+		}
+
+		[TestMethod]
+		public void UnknownCustomDatePattern()
+		{
+			NamingPattern.TryParse("{Date:ABC}", out np);
+			Assert.AreEqual(DateTime.Now.ToString("dd.MM.yyy_HH.mm"), np.Apply(game));
 		}
 	}
 }
