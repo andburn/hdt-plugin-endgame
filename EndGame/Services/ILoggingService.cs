73d928c6c7f047c9b4a06c1e7c442e95595e18b2
Andrew Burnett <andrew@andburn.info>
2016-07-07 20:41:03 +0100

    Add logging wrapper

---
+++ EndGame/Services/ILoggingService.cs
@@ -0,0 +1,17 @@
+﻿namespace HDT.Plugins.EndGame.Services
+{
+	public interface ILoggingService
+	{
+		void Error(string message);
+
+		void Error(object obj);
+
+		void Info(string message);
+
+		void Info(object obj);
+
+		void Debug(string message);
+
+		void Debug(object obj);
+	}
+}
---
+++ EndGame/Services/TrackerLogger.cs
@@ -0,0 +1,37 @@
+﻿using Hearthstone_Deck_Tracker.Utility.Logging;
+
+namespace HDT.Plugins.EndGame.Services
+{
+	public class TrackerLogger : ILoggingService
+	{
+		public void Debug(object obj)
+		{
+			Log.Debug(obj.ToString());
+		}
+
+		public void Debug(string message)
+		{
+			Log.Debug(message);
+		}
+
+		public void Error(object obj)
+		{
+			Log.Error(obj.ToString());
+		}
+
+		public void Error(string message)
+		{
+			Log.Error(message);
+		}
+
+		public void Info(object obj)
+		{
+			Log.Info(obj.ToString());
+		}
+
+		public void Info(string message)
+		{
+			Log.Info(message);
+		}
+	}
+}
