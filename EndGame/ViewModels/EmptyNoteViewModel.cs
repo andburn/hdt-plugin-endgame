49460674dd028a7c1359212884fefc7d65d23dd7
Andrew Burnett <andrew@andburn.info>
2017-02-19 14:32:21 +0000

    Add empty note view

---
+++ EndGame/ViewModels/EmptyNoteViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace HDT.Plugins.EndGame.ViewModels
+{
+	public class EmptyNoteViewModel : ViewModelBase
+	{
+		private string _message;
+
+		public string Message
+		{
+			get { return _message; }
+			set { Set(() => Message, ref _message, value); }
+		}
+
+		public EmptyNoteViewModel()
+		{
+			Message = "Not Available";
+		}
+	}
+}
---
+++ EndGame/Views/EmptyNoteView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace HDT.Plugins.EndGame.Views
+{
+	public partial class EmptyNoteView : UserControl
+	{
+		public EmptyNoteView()
+		{
+			InitializeComponent();
+		}
+	}
+}
