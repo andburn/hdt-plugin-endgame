3f2a7ed6508302c54b5788c121b1015da2f43734
Andrew Burnett <andrew@andburn.info>
2017-07-20 10:29:39 +0100

    Add ArchetypeRecord with tests

---
+++ EndGame.Tests/Models/ArchetypeRecordTest.cs
@@ -0,0 +1,71 @@
+﻿using HDT.Plugins.Common.Enums;
+using HDT.Plugins.EndGame.Models;
+using NUnit.Framework;
+
+namespace HDT.Plugins.EndGame.Tests.Models
+{
+	[TestFixture]
+	public class ArchetypeRecordTest
+	{
+		private ArchetypeRecord record;
+
+		[SetUp]
+		public void Setup()
+		{
+			record = new ArchetypeRecord();
+		}
+
+		[Test]
+		public void DefaultCtor_HasDefaultValues()
+		{
+			Assert.That(record.Name, Is.EqualTo(ArchetypeRecord.DefaultName));
+			Assert.That(record.Klass, Is.EqualTo(PlayerClass.ALL));
+			Assert.That(record.TotalWins, Is.EqualTo(0));
+			Assert.That(record.TotalLosses, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void ParamCtor_TakesNameAndClass()
+		{
+			var r = new ArchetypeRecord("Control", PlayerClass.MAGE);
+			Assert.That(r.Name, Is.EqualTo("Control"));
+			Assert.That(r.Klass, Is.EqualTo(PlayerClass.MAGE));
+		}
+
+		[Test]
+		public void WinRate_IsZero_WithZeroGames()
+		{
+			Assert.That(record.WinRate, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void WinRate_IsZero_WithZeroWinsAndAnyLosses()
+		{
+			record.TotalLosses = 4;
+			Assert.That(record.WinRate, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void WinRate_IsCorrect_ForAnyWinsAndNoLosses()
+		{
+			record.TotalWins = 1;
+			Assert.That(record.WinRate, Is.EqualTo(100.0f));
+		}
+
+		[Test]
+		public void WinRate_IsCorrect_ForAnyWinsAndAnyLosses()
+		{
+			record.TotalWins = 6;
+			record.TotalLosses = 4;
+			Assert.That(record.WinRate, Is.EqualTo(60.0f));
+		}
+
+		[Test]
+		public void WinRateText_IsCorrect_ForAnyWinsAndAnyLosses()
+		{
+			record.TotalWins = 3;
+			record.TotalLosses = 6;
+			Assert.That(record.WinRateText, Is.EqualTo("33%"));
+		}
+	}
+}
---
+++ EndGame/Models/ArchetypeRecord.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using GalaSoft.MvvmLight;
+using HDT.Plugins.Common.Enums;
+
+namespace HDT.Plugins.EndGame.Models
+{
+	public class ArchetypeRecord : ObservableObject
+	{
+		public static readonly string DefaultName = "Unknown";
+
+		public string Name { get; set; }
+		public PlayerClass Klass { get; set; }
+		public int TotalWins { get; set; }
+		public int TotalLosses { get; set; }
+
+		public float WinRate
+		{
+			get { return CalcWinRate(); }
+		}
+
+		public string WinRateText
+		{
+			get { return ((int)Math.Round(WinRate)).ToString() + "%"; }
+		}
+
+		public ArchetypeRecord()
+		{
+			Name = DefaultName;
+			Klass = PlayerClass.ALL;
+		}
+
+		public ArchetypeRecord(string name, PlayerClass klass)
+		{
+			Name = name;
+			Klass = klass;
+		}
+
+		private float CalcWinRate()
+		{
+			var total = (float)(TotalWins + TotalLosses);
+			if (total <= 0)
+				return 0;
+			return TotalWins / total * 100;
+		}
+
+		public override string ToString()
+		{
+			return $"{Name} ({Klass}) {TotalWins}-{TotalLosses} ({WinRateText})";
+		}
+	}
+}
