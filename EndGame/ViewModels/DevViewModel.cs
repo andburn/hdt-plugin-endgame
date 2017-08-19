970bd5bcd7b882c9bb225827423b3a2e4043939e
Andrew Burnett <andrew@andburn.info>
2017-08-19 22:46:53 +0100

    Implement basic dev view

--- EndGame/ViewModels/DevViewModel.cs
+++ EndGame/ViewModels/DevViewModel.cs
@@ -1,14 +1,25 @@
-﻿using HDT.Plugins.Common.Enums;
+﻿using GalaSoft.MvvmLight.Command;
 using HDT.Plugins.Common.Models;
 using HDT.Plugins.Common.Services;
+using HDT.Plugins.Common.Utils;
 using HDT.Plugins.EndGame.Models;
 using HDT.Plugins.EndGame.Utilities;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using HDTCard = Hearthstone_Deck_Tracker.Hearthstone.Card;
+using HDTDb = Hearthstone_Deck_Tracker.Hearthstone.Database;
+
+// TODO dev mode resize window
+// TODO clear spare cards on start
+// TODO when not watching select should show deck list
+// TODO replace stars with numbers
+// TODO add tooltips
+// TODO sort by count then mana
+// TODO re compare archs on all updates?
+// TODO stop should reset
 
 namespace HDT.Plugins.EndGame.ViewModels
 {
@@ -20,6 +31,8 @@ namespace HDT.Plugins.EndGame.ViewModels
 		private ILoggingService _log;
 
 		public ObservableCollection<MatchResult> Decks { get; set; }
+
+		public ObservableCollection<Card> SpareCards { get; set; }
 
 		private MatchResult _selectedDeck;
 
@@ -37,6 +50,43 @@ namespace HDT.Plugins.EndGame.ViewModels
 			set { Set(() => IsLoadingDecks, ref _isLoadingDecks, value); }
 		}
 
+		private List<ArchetypeDeck> _archetypes;
+
+		private List<ArchetypeDeck> ArchetypeDecks
+		{
+			get
+			{
+				if (_archetypes == null || _archetypes.Count <= 0)
+					_archetypes = _repository
+						.GetAllDecksWithTag(Strings.ArchetypeTag)
+						.Select(d => new ArchetypeDeck(d))
+						.ToList();
+				return _archetypes;
+			}
+			set
+			{
+				_archetypes = value;
+			}
+		}
+
+		private bool _isWatching;
+
+		public bool IsWatching
+		{
+			get { return _isWatching; }
+			set { Set(() => IsWatching, ref _isWatching, value); }
+		}
+
+		private string _watchButtonText;
+
+		public string WatchButtonText
+		{
+			get { return _watchButtonText; }
+			set { Set(() => WatchButtonText, ref _watchButtonText, value); }
+		}
+
+		public RelayCommand WatchButtonClick { get; private set; }
+
 		public DevViewModel()
 			: this(EndGame.Data, EndGame.Logger)
 		{
@@ -46,86 +96,131 @@ namespace HDT.Plugins.EndGame.ViewModels
 			: base(track, logger)
 		{
 			Cards = new ObservableCollection<Card>();
+			SpareCards = new ObservableCollection<Card>();
 			Decks = new ObservableCollection<MatchResult>();
 			BindingOperations.EnableCollectionSynchronization(Decks, _deckLock);
 			_repository = track;
 			_log = logger;
-			PropertyChanged += NoteViewModel_PropertyChanged;
+			WatchButtonClick = new RelayCommand(() => ToggleWatching());
+			IsWatching = false;
+			WatchButtonText = "Watch";
+			PropertyChanged += DevViewModel_PropertyChanged;
+			// Set events
+			Hearthstone_Deck_Tracker.API.GameEvents.OnOpponentPlay.Add(x => Played(x));
+			Hearthstone_Deck_Tracker.API.GameEvents.OnTurnStart.Add(x => TurnStart(x));
+			Hearthstone_Deck_Tracker.API.GameEvents.OnGameStart.Add(() => GameStart());
 		}
 
 		public override async Task Update()
 		{
-			IsNoteFocused = true;
-			IsLoadingDecks = true;
-			Note = _repository.GetGameNote()?.ToString();
+			IsNoteFocused = false;
+			Deck deck = _repository.GetOpponentDeckLive();
+			PlayerClass = deck.Class.ToString();
 
-			Deck deck = _repository.GetOpponentDeckLive();;			
-
-			Decks.Clear();
-			if (Cards.Count <= 0)
+			if (deck.Cards.Count <= 0)
 			{
 				EndGame.Logger.Debug("DevVM: Opponent deck is empty, skipping");
 				IsLoadingDecks = false;
 				return;
 			}
-			await Task.Run(() =>
+			// if watching don't rerun archtype comparison
+			if (!IsWatching)
 			{
-				var alldecks = _repository
-					.GetAllDecksWithTag(Strings.ArchetypeTag)
-					.Select(d => new ArchetypeDeck(d))
-					.ToList();
-				var format = EndGame.Data.GetGameFormat();
-				var results = ViewModelHelper.MatchArchetypes(format, deck, alldecks);
-				results.ForEach(r => Decks.Add(r));
-				results.ForEach(r => _log.Debug($"Archetype: ({r.Similarity}, {r.Containment}) " +
-					$"{r.Deck.DisplayName} ({r.Deck.Class})"));				
-				IsLoadingDecks = false;
-			});
+				Decks.Clear();
+				IsLoadingDecks = true;
+				await Task.Run(() =>
+				{
+					var results = ViewModelHelper.MatchArchetypes(
+							EndGame.Data.GetGameFormat(), deck, ArchetypeDecks);
+					results.ForEach(r => Decks.Add(r));
+					results.ForEach(r => _log.Debug($"Archetype: ({r.Similarity}, {r.Containment}) " +
+						$"{r.Deck.DisplayName} ({r.Deck.Class})"));
+					IsLoadingDecks = false;
+				});
+			}
 
-			var firstDeck = Decks.FirstOrDefault();
-			if (firstDeck != null && firstDeck.Similarity > MatchResult.THRESHOLD)
-				DeckSelected(firstDeck);
+			var archDeck = SelectedDeck ?? Decks.FirstOrDefault();
 
-			Cards.Clear();
-			deck.Cards.ForEach(c => Cards.Add(c));
-			PlayerClass = deck.Class.ToString();
+			if (IsWatching && archDeck != null)
+			{
+				PlayerClass = archDeck.Deck.Name.ToUpper();
+				Common.Common.Log.Debug($"DevVM: watching & deck selected '{archDeck.Deck.Name}'");
+				var lookup = deck.Cards.ToDictionary(x => x.Id);
+
+				Cards.Clear();
+				SpareCards.Clear();
+				var found = new List<string>();
+				foreach (var card in archDeck.Deck.Cards)
+				{
+					var c = HDTDb.GetCardFromId(card.Id);
+					if (lookup.ContainsKey(card.Id))
+					{
+						// TODO handle found case of having 1 copy in archetype and 2 played
+						found.Add(card.Id);
+						c.Count = card.Count - lookup[card.Id].Count;
+						Common.Common.Log.Debug($"DevVM: {card.Id} count was {card.Count} now {c.Count}");
+					}
+					Cards.Add(new Card(c.Id, c.Name, c.Count, c.Background));
+				}
+				found.ForEach(k => lookup.Remove(k));
+				lookup.Values.ToList().ForEach(v => SpareCards.Add(v));
+			}
+			else
+			{
+				// just show opponent deck straight
+				Common.Common.Log.Debug($"DevVM: not watching, showing played cards");
+				Cards.Clear();
+				deck.Cards.ForEach(c => Cards.Add(c));
+			}
+		}
+
+		private void ToggleWatching()
+		{
+			IsWatching = !IsWatching;
+			Common.Common.Log.Debug($"DevVM: toggled {IsWatching}");
+			WatchButtonText = IsWatching ? "Stop" : "Watch";
+			Common.Common.Log.Debug($"DevVM: text {WatchButtonText}");
+		}
+
+		private void Played(HDTCard c)
+		{
+			Common.Common.Log.Debug($"DevVM: Played {c.Name}");
+			EventTrigger();
+		}
+
+		private void TurnStart(Hearthstone_Deck_Tracker.Enums.ActivePlayer p)
+		{
+			Common.Common.Log.Debug($"DevVM: TurnStart {p}");
+			EventTrigger();
+		}
+
+		private void GameStart()
+		{
+			Common.Common.Log.Debug($"DevVM: GameStart");
+			_archetypes.Clear();
+			if (IsWatching)
+				ToggleWatching();
+		}
+
+		private void EventTrigger()
+		{
+			Common.Common.Log.Debug($"DevVM: Event Triggered");
+			Update().Forget();
 		}
 
 		private void DeckSelected(MatchResult item)
 		{
 			SelectedDeck = item ?? SelectedDeck;
-			if (SelectedDeck == null)
-				return;
-			AddDeckToNote(SelectedDeck.Deck.Name);
+			if (SelectedDeck != null)
+				Update().Forget();
 		}
 
-		private void AddDeckToNote(string text)
-		{
-			if (string.IsNullOrWhiteSpace(text))
-				return;
-
-			Note = Note ?? string.Empty;
-			const string regex = "\\[(?<tag>(.*?))\\]";
-			var match = Regex.Match(Note, regex);
-			if (match.Success)
-			{
-				var tag = match.Groups["tag"].Value;
-				_log.Debug($"NoteVM: Replacing '{tag}' with {text}'");
-				Note = Note.Replace(match.Value, $"[{text}]");
-			}
-			else
-			{
-				_log.Debug($"NoteVM: Adding '{text}' to note");
-				Note = $"[{text}] {Note}";
-			}
-		}
-
-		private void NoteViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		private void DevViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
 		{
 			if (e.PropertyName == "SelectedDeck")
 			{
 				DeckSelected(SelectedDeck);
-				_log.Debug($"NoteVM: DeckSelected ({SelectedDeck.Deck.DisplayName})");
+				_log.Debug($"DevVM: DeckSelected ({SelectedDeck.Deck.DisplayName})");
 			}
 		}
 	}
